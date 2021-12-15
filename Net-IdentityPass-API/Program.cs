1702d1042b8a2c62fb0e21766acd365dbb8d5bb8
TobbyJay <umohjunior96@gmail.com>
2021-12-15 23:44:07 +0100

    swiitch environment back to production

--- Net-IdentityPass-API/Program.cs
+++ Net-IdentityPass-API/Program.cs
@@ -46,7 +46,7 @@ builder.Services.AddEntityFrameworkNpgsql().AddDbContext<AppDbContext>(options =
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
