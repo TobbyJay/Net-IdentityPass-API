8e3acfec1965ac7f7a90974a7b938ca0717cbde2
TobbyJay <umohjunior96@gmail.com>
2021-12-17 13:57:17 +0100

    minor changes

--- DTOs/Requests/VerificationRequest.cs
+++ DTOs/Requests/VerificationRequest.cs
@@ -8,18 +8,18 @@ namespace DTOs.Requests
 {
     public class VerificationRequest
     {
-        public string UserReferenceId { get; set; }
-        public string Type { get; set; }
-        public string BvnNumber { get; set; }
-        public string NiNumber { get; set; }
-        public string ViNumber { get; set; }
-        public string LastName { get; set; }
-        public string State { get; set; }
-        public string PhoneNumber { get; set; }
-        public string FirstName { get; set; }
-        public string Dob { get; set; }
-        public string FrscNumber { get; set; }
-        public string Url { get; set; }
+        public string? UserReferenceId { get; set; }
+        public string? Type { get; set; }
+        public string? BvnNumber { get; set; }
+        public string? NiNumber { get; set; }
+        public string? ViNumber { get; set; }
+        public string? LastName { get; set; }
+        public string? State { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? Dob { get; set; }
+        public string? FrscNumber { get; set; }
+        public string? Url { get; set; }
 
     }
 }
