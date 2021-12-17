c5db373190e2d1618aaf59c732f2312d66c116e8
TobbyJay <umohjunior96@gmail.com>
2021-12-17 11:25:55 +0100

    redesign dashboard

--- Net-IdentityPass-API/Program.cs
+++ Net-IdentityPass-API/Program.cs
@@ -68,7 +68,7 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Account}/{action=LogIn}/{id?}");
+        pattern: "{controller=user}/{action=settings}/{id?}");
 });
 app.MapControllers();
 
